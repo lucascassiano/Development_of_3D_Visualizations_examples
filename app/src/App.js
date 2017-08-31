47dbcd1a8c8284766bbac7353aa04b39371d3319
Lucas Cassiano Silva <lucascassiano@Lucass-MBP.mshome.net>
2017-08-31 16:57:15 -0300

    mais coisa

--- app/src/App.js
+++ app/src/App.js
@@ -5,16 +5,29 @@ import BarraLateral from './components/BarraLateral';
 import database from './data/database.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      currExam : null
+    }
 
-  HandleExam (exam) {
-    console.log("Exame foi trocado para ",exam);
+    this.HandleExam = this.HandleExam.bind(this);
+  }
+
+  HandleExam(exam) {
+   
+    this.setState({
+      currExam: exam
+    });
+
+    console.log("Exame foi trocado para ", this.state.currExam);
   }
 
   render() {
-    console.log("database",database);
+    console.log("database", database);
     return (
       <div className="App">
-        <BarraLateral title="3D App" database={database} clickCallback = {this.HandleExam}/>
+        <BarraLateral title="3D App" database={database} clickCallback={this.HandleExam} />
       </div>
     );
   }
