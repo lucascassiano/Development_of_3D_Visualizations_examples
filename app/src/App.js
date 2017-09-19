46f5ef36e53332ac837b928c5ef26a0715f5273b
Lucas Cassiano Silva <lucascassiano@Lucass-MacBook-Pro.local>
2017-09-19 16:30:34 -0300

    examples

--- app/src/App.js
+++ app/src/App.js
@@ -3,19 +3,29 @@ import React, { Component } from 'react';
 import './App.css';
 import BarraLateral from './components/BarraLateral';
 import database from './data/database.json';
+import Sidebar from 'react-sidebar';
+import {Nav, NavItem, navInstance} from 'react-bootstrap';
+import ObjectViewer from './components/ObjectViewer';
+import Simple from './components/Simple';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      currExam : null
+    this.state = {
+      currExam: null,
+      sidebarOpen: false
     }
 
     this.HandleExam = this.HandleExam.bind(this);
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+  }
+
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
   }
 
   HandleExam(exam) {
-   
+
     this.setState({
       currExam: exam
     });
@@ -23,11 +33,30 @@ class App extends Component {
     console.log("Exame foi trocado para ", this.state.currExam);
   }
 
+   handleSelect(selectedKey) {
+    alert('selected ' + selectedKey);
+  }
+  
+
+  
+
+  
+
   render() {
     console.log("database", database);
+    var sidebarContent = <b>Sidebar content</b>;
+    const navInstance = (
+      <Nav bsStyle="pills" activeKey={1} onSelect={this.handleSelect}>
+        <NavItem eventKey={1} href="/home">NavItem 1 content</NavItem>
+        <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
+        <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
+      </Nav>
+    );
+
     return (
       <div className="App">
-        <BarraLateral title="3D App" database={database} clickCallback={this.HandleExam} />
+      <Simple width={800} height={600}/>
+        
       </div>
     );
   }
